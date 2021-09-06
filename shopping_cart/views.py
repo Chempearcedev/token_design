e634c69bf25ee5f6910a13329ca3997394343171
Chempearcedev <chempearce1@hotmail.com>
2021-09-06 13:53:20 +0000

    Continued with shopping cart functionality

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -18,5 +18,4 @@ def add_to_cart(request, item_id):
         shopping_cart[item_id] = quantity
 
     request.session['shopping_cart'] = shopping_cart
-    print(request.session['shopping_cart'])
     return redirect(redirect_url)
