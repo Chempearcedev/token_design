dfb95322e26af38aaa272defcc54d34819d4b21d
Chempearcedev <chempearce1@hotmail.com>
2021-09-10 08:19:31 +0000

    Included checkout success reasoning

--- checkout/signals.py
+++ checkout/signals.py
@@ -8,5 +8,6 @@ def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
+
     instance.order.update_total()
