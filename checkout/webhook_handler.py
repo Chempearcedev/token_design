5905d33283bd3741395a7ed3dc8d78189d8c73b4
Chempearcedev <chempearce1@hotmail.com>
2021-09-13 08:28:18 +0000

    Edited indentation in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -16,22 +16,22 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-        def _send_confirmation_email(self, order):
+    def _send_confirmation_email(self, order):
 
-            cust_email = order.email
-            subject = render_to_string(
-                'checkout/confirmation_emails/confirmation_email_subject.txt',
-                {'order': order})
-            body = render_to_string(
-                'checkout/confirmation_emails/confirmation_email_body.txt',
-                {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
 
-        send_mail(
+    send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )
+    )
 
     def handle_event(self, event):
 
